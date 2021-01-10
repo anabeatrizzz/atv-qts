bb5cff07d9ab678517ba4d2ec0d37d8df9b6bff7
anabeatrizzz <anabeatriz.augusto06@yahoo.com>
2021-01-10 11:50:16 -0300

    enviando

---
+++ README.md
@@ -0,0 +1,4 @@
+🧐️ __Fato interessante__: Este código foi usado para explicar a resolução da tarefa no dia 20/10/2020.
+
+### Romanos/NumerosRomanos.cs: Classe que será testada.
+### RomanosTeste/TesteUnitario1.cs: Teste unitário que indicará possíveis erros.
---
+++ RomanosTeste/TesteUnitario1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Romanos;
+
+namespace RomanosTeste {
+  [TestClass]
+  public class TesteUnitario1 {
+    [TestInitialize]
+    public void IniciarTeste() {
+      
+    }
+    
+    [TestMethod]
+    public void TesteMilhar() {
+      bool resultado = NumerosRomanos.ConverterParaRomano(1000);
+      Assert.IsTrue(resultado, "Algo errado no if do método ConverterParaRomano");
+    }
+
+    [TestMethod]
+    public void TesteCentena() {
+      bool resultado = NumerosRomanos.ConverterParaRomano(100);
+      Assert.IsTrue(resultado, "Algo errado no primeiro else if do método ConverterParaRomano");
+    }
+
+    [TestMethod]
+    public void TesteDezena() {
+      bool resultado = NumerosRomanos.ConverterParaRomano(10);
+      Assert.IsTrue(resultado, "Algo errado no segundo else if do método ConverterParaRomano");
+    }
+
+    [TestMethod]
+    public void TesteUnidade() {
+      bool resultado = NumerosRomanos.ConverterParaRomano(1);
+      Assert.IsTrue(resultado, "Algo errado no ultimo else if do método ConverterParaRomano");
+    }
+
+    [TestCleanup]
+    public void FinalizarTeste() {
+    
+    }
+  }
+}
